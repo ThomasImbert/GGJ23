026ea0b61cd54814c4fb00998253b4f5e65ec69f
Thomas Imbert <thomas.imbert@pm.me>
2023-02-05 12:58:28 +0100

    New gameLogic script First Build Camera rays fix

--- GGJ3_Koneksion_Unity/Assets/Scripts/ClickFunction.cs
+++ GGJ3_Koneksion_Unity/Assets/Scripts/ClickFunction.cs
@@ -1,14 +1,112 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class ClickFunction : MonoBehaviour
 {
-    public GameObject myTarget;
+    public Animator cameraAnimator;
+    public Camera cam;
 
-    void OnMouseDown()
+    void Start()
     {
-        Debug.Log(myTarget.gameObject.name);
-        AkSoundEngine.PostEvent("Play_TRACK_1", this.gameObject);
     }
+
+    void Update()
+    {
+        DetectObjectWithRaycast();
+    }
+
+    public void DetectObjectWithRaycast()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.name == ("GameStart")) {
+
+                    Debug.Log($"{hit.collider.name} Detected",
+                        hit.collider.gameObject);
+                    AkSoundEngine.PostEvent("Play_ROOTS", this.gameObject);
+                    cameraAnimator.SetBool("StartGame", true);
+                }
+
+                if (hit.collider.gameObject.name == ("Outlet01"))
+                {
+
+                    Debug.Log($"{hit.collider.name} Detected",
+                        hit.collider.gameObject);
+                    // AkSoundEngine.PostEvent("Play_ROOTS", this.gameObject);
+                    cameraAnimator.SetBool("Dezoom01", true);
+                }
+
+                if (hit.collider.gameObject.name == ("Outlet02"))
+                {
+
+                    Debug.Log($"{hit.collider.name} Detected",
+                        hit.collider.gameObject);
+                    //AkSoundEngine.PostEvent("Play_ROOTS", this.gameObject);
+                    //cameraAnimator.SetBool("StartGame", true);
+                }
+
+                if (hit.collider.gameObject.name == ("Outlet03"))
+                {
+
+                    Debug.Log($"{hit.collider.name} Detected",
+                        hit.collider.gameObject);
+                    //AkSoundEngine.PostEvent("Play_ROOTS", this.gameObject);
+                    cameraAnimator.SetBool("Dezoom02", true);
+                }
+
+                if (hit.collider.gameObject.name == ("Outlet04"))
+                {
+
+                    Debug.Log($"{hit.collider.name} Detected",
+                        hit.collider.gameObject);
+                    //AkSoundEngine.PostEvent("Play_ROOTS", this.gameObject);
+                    //cameraAnimator.SetBool("StartGame", true);
+                }
+
+                if (hit.collider.gameObject.name == ("Outlet05"))
+                {
+
+                    Debug.Log($"{hit.collider.name} Detected",
+                        hit.collider.gameObject);
+                    //AkSoundEngine.PostEvent("Play_ROOTS", this.gameObject);
+                    cameraAnimator.SetBool("Dezoom03", true);
+                }
+
+                if (hit.collider.gameObject.name == ("Outlet06"))
+                {
+
+                    Debug.Log($"{hit.collider.name} Detected",
+                        hit.collider.gameObject);
+                    //AkSoundEngine.PostEvent("Play_ROOTS", this.gameObject);
+                    //cameraAnimator.SetBool("StartGame", true);
+                }
+
+                if (hit.collider.gameObject.name == ("Outlet07"))
+                {
+
+                    Debug.Log($"{hit.collider.name} Detected",
+                        hit.collider.gameObject);
+                    //AkSoundEngine.PostEvent("Play_ROOTS", this.gameObject);
+                    //cameraAnimator.SetBool("DezoomFinal", true);
+                }
+
+                if (hit.collider.gameObject.name == ("Outlet08"))
+                {
+
+                    Debug.Log($"{hit.collider.name} Detected",
+                        hit.collider.gameObject);
+                    //AkSoundEngine.PostEvent("Play_ROOTS", this.gameObject);
+                    cameraAnimator.SetBool("DezoomFinal", true);
+                }
+            }
+        }
+    }
+
 }
